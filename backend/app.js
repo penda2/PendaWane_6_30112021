5fa7324981d32aef20e01153e531d2ee73d420f5
Penda Wane <pendawane@yahoo.fr>
2022-01-24 12:23:50 +0100

    configuration et connection à la base de données

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const path = require('path');
+mongoose.connect('mongodb+srv://endGame:<password>@cluster0.b6wn9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 const app = express();
-app.use((req, res) => {
-    res.json({ message: 'Marchera ?'});
-});
+
 module.exports = app;
